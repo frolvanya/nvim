f9452fbe8647912865b1517606ddbe8bfe5f4eef
Ivan Frolov <frolvanya@gmail.com>
2024-07-30 20:49:16 -0400

    feat: added codelens support

--- lua/frolik/plugins/lsp.lua
+++ lua/frolik/plugins/lsp.lua
@@ -11,6 +11,34 @@ local function show_documentation()
     end
 end
 
+local function setup_codelens_refresh(client, bufnr)
+    local status_ok, codelens_supported = pcall(function()
+        return client.supports_method "textDocument/codeLens"
+    end)
+    if not status_ok or not codelens_supported then
+        return
+    end
+    local group = "lsp_code_lens_refresh"
+    local cl_events = { "BufEnter", "InsertLeave" }
+    local ok, cl_autocmds = pcall(vim.api.nvim_get_autocmds, {
+        group = group,
+        buffer = bufnr,
+        event = cl_events,
+    })
+
+    if ok and #cl_autocmds > 0 then
+        return
+    end
+    vim.api.nvim_create_augroup(group, { clear = false })
+    vim.api.nvim_create_autocmd(cl_events, {
+        group = group,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.codelens.refresh { bufnr = bufnr }
+        end,
+    })
+end
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -91,6 +119,8 @@ return {
                             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
                         end, "Toggle Inlay Hints")
                     end
+
+                    setup_codelens_refresh(client, event.buf)
                 end,
             })
 
