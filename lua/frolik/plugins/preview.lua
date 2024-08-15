76b7410f90fc5451bbdbf9661c1f8406e1ca4e1b
Ivan Frolov <frolvanya@gmail.com>
2024-08-15 11:44:02 -0400

    feat: added plugin for markdown renderer

--- lua/frolik/plugins/preview.lua
+++ lua/frolik/plugins/preview.lua
@@ -1,5 +1,13 @@
 return {
     {
+        "OXY2DEV/markview.nvim",
+        ft = "markdown",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
+    {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
