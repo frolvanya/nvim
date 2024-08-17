b36e9fb5e785fb9ccecf4f75fd9ff2582f98502b
Ivan Frolov <frolvanya@gmail.com>
2024-08-17 02:33:56 -0400

    chore: removed useless info from custom health checker

--- lua/frolik/health.lua
+++ lua/frolik/health.lua
@@ -35,13 +35,7 @@ end
 
 return {
     check = function()
-        vim.health.start "kickstart.nvim"
-
-        vim.health.info [[NOTE: Not every warning is a 'must-fix' in `:checkhealth`
-
-  Fix only warnings for plugins and languages you intend to use.
-    Mason will give warnings for languages that are not installed.
-    You do not need to install, unless you want to use those languages!]]
+        vim.health.start "frolik.nvim"
 
         local uv = vim.uv or vim.loop
         vim.health.info("System Information: " .. vim.inspect(uv.os_uname()))
