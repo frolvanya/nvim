961df1a6b18b0ed0f1258f45c138894fe322a39b
Ivan Frolov <frolvanya@gmail.com>
2024-12-02 17:10:05 -0500

    chore: updated `copilotchat` branch

--- lua/frolik/plugins/copilot.lua
+++ lua/frolik/plugins/copilot.lua
@@ -77,7 +77,6 @@ return {
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
         dependencies = {
             { "zbirenbaum/copilot.lua" },
             { "nvim-lua/plenary.nvim" },
