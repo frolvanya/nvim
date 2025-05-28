03c8388fd208b7a5b4504d0d69b58413a5bb2f70
Ivan Frolov <frolvanya@gmail.com>
2025-05-28 00:22:52 -0400

    fix: incsearch highlight color

--- lua/frolik/colorscheme.lua
+++ lua/frolik/colorscheme.lua
@@ -3,6 +3,7 @@ local colors = require "frolik.colors"
 vim.cmd("autocmd ColorScheme * highlight NormalFloat guifg=" .. colors.nord5 .. " guibg=NONE")
 vim.cmd("autocmd ColorScheme * highlight FloatBorder guifg=" .. colors.nord5 .. " guibg=NONE")
 vim.cmd("autocmd ColorScheme * highlight LspInfoBorder guifg=" .. colors.nord5 .. " guibg=NONE")
+vim.cmd("autocmd ColorScheme * highlight IncSearch guifg=" .. colors.nord1 .. " guibg=" .. colors.nord8)
 
 vim.cmd "autocmd ColorScheme * highlight link LspInlayHint Comment"
 vim.cmd "autocmd ColorScheme * highlight StatusLine ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE"
