d9db63344108fb78783634fecd64709973e3968d
Ivan Frolov <frolvanya@gmail.com>
2025-07-25 12:30:45 +0200

    fix: removed `grt` mapping to fix `gr`

--- lua/frolik/plugins/lsp.lua
+++ lua/frolik/plugins/lsp.lua
@@ -109,6 +109,7 @@ return {
                     pcall(vim.keymap.del, "n", "gri")
                     pcall(vim.keymap.del, "n", "grn")
                     pcall(vim.keymap.del, "n", "grr")
+                    pcall(vim.keymap.del, "n", "grt")
                     map("gl", function()
                         local float = vim.diagnostic.config().float
 
